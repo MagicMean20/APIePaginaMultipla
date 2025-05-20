7dbe345504cc7fddd17bed5ff0f768b2c403b8b2
Cleonilson <Cleonilson@DESKTOP-0FSUH0V>
2025-05-20 16:54:34 -0300

    Adicionar arquivos de projeto.

---
+++ APIePaginaMultipla/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace APIePaginaMultipla
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ APIePaginaMultipla/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace APIePaginaMultipla
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
